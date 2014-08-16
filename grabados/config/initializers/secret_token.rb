fb522706a1915a2cead87ccaf879b2d2598654ad
Diego Rodriguezg <diego@mbp-de-diego.lan>
2014-08-16 02:17:14 -0400

    first commit <3

---
+++ grabados/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grabados::Application.config.secret_key_base = 'ff51725bd22ebf073a931668f30dabb025165f7bdf66d53d377141c7b9f7616070155481c77bc38d1b656a7fbdce17fbdfc98b971d973779c61af501342af2b0'
---
+++ grabados/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grabados::Application.config.session_store :cookie_store, key: '_grabados_session'
